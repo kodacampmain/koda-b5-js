d4db4b91d75e9e1e0dabf9f36542a6dcc46faa4a
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-11-06 13:34:56 +0700

    feat:literal

---
+++ 6.literals.js
@@ -0,0 +1,16 @@
+// backtick = kutip balik
+const myName = `Koda
+Tech    Academy`;
+console.log(myName);
+
+const firstName = "Andi";
+const age = 30;
+console.log(`Nama saya ${firstName}
+Umur saya ${age + 5}`);
+
+/**
+ * Nama saya NamaKalian
+ * Domisili saya di KotaKalian
+ * Penghasilan tahunan saya adalah PenghasilanTahunan
+ * Penghasilan bulanan adalah ...
+ */
